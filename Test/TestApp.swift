114e939ce8646ce1e8a236e6d22fc0ff69d07fc1
Laurent Davery <Laurent.Davery@gmail.com>
2022-08-26 11:36:57 +0400

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Laurent Davery on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
